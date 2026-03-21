cb89b4caf0e38e032228a7c0ed28545ac9a2c20b
AshBringer4eg <ashbringer4eg@gmail.com>
2026-03-21 12:59:26 +0100

    Routes change according to https://reactrouter.com/6.30.3/upgrading/future#v7_relativesplatpath

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
@@ -7,19 +7,25 @@ import Index from "./pages/Index.tsx";
 import NotFound from "./pages/NotFound.tsx";
 
 const queryClient = new QueryClient();
+const router = createBrowserRouter(
+  [
+    { path: "/", element: <Index /> },
+    { path: "*", element: <NotFound /> },
+  ],
+  {
+    basename: import.meta.env.BASE_URL,
+    future: {
+      v7_relativeSplatPath: true,
+    },
+  },
+);
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter basename={import.meta.env.BASE_URL}>
-        <Routes>
-          <Route path="/" element={<Index />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
+      <RouterProvider router={router} future={{ v7_startTransition: true }} />
     </TooltipProvider>
   </QueryClientProvider>
 );
